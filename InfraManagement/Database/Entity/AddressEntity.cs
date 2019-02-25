ae6f60f010ece434b4066e7efb85049eaac217ef
Gags <gags_bd@hotmail.com>
2019-02-25 09:06:10 -0500

    Added database files

---
+++ InfraManagement/Database/Entity/AddressEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace InfraManagement.Database.Entity
+{
+    public class AddressEntity
+    {
+        public string Zip { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
